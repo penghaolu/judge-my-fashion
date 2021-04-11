c78f118363b5acfdcc7267b9614440220f4f3298
Peng Hao Lu <penglu@stanford.edu>
2021-04-11 03:18:22 -0400

    Update Upload.js

--- frontend/src/components/Upload.js
+++ frontend/src/components/Upload.js
@@ -1,55 +1,64 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Upload.css";
 
-class Upload extends Component {
-  state = {
-    // Initially, no file is selected
-    selectedFile: null,
-  };
+function Upload() {
+  const [selectedFile, setSelectedFile] = useState(null);
 
   // On file select (from the pop up)
-  onFileChange = (event) => {
+  function onFileChange(event) {
     // Update the state
-    this.setState({ selectedFile: event.target.files[0] });
-  };
+    setSelectedFile(event.target.files[0]);
+  }
 
   // On file upload (click the upload button)
-  onFileUpload = () => {
+  function onFileUpload() {
     // Create an object of formData
     const formData = new FormData();
 
     // Update the formData object
-    formData.append(
-      "myFile",
-      this.state.selectedFile,
-      this.state.selectedFile.name
-    );
+    formData.append("myFile", selectedFile, selectedFile.name);
 
     // Details of the uploaded file
-    console.log(this.state.selectedFile);
+    console.log(selectedFile);
 
     // Request made to the backend api
     // Send formData object
-    axios.post("api/uploadfile", formData);
-  };
+    axios
+      .post("/upload", formData)
+      .then((response) => {
+        console.log(response);
+        getResults();
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+      });
+  }
+
+  function getResults() {
+    axios
+      .get("/results")
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+      });
+  }
 
   // File content to be displayed after
   // file upload is complete
-  fileData = () => {
-    if (this.state.selectedFile) {
+  function fileData() {
+    if (selectedFile) {
       return (
         <div className="general">
           <h2>File Details:</h2>
 
-          <p>File Name: {this.state.selectedFile.name}</p>
+          <p>File Name: {selectedFile.name}</p>
 
-          <p>File Type: {this.state.selectedFile.type}</p>
+          <p>File Type: {selectedFile.type}</p>
 
-          <p>
-            Last Modified:{" "}
-            {this.state.selectedFile.lastModifiedDate.toDateString()}
-          </p>
+          <p>Last Modified: {selectedFile.lastModifiedDate.toDateString()}</p>
         </div>
       );
     } else {
@@ -60,23 +69,21 @@ class Upload extends Component {
         </div>
       );
     }
-  };
-
-  render() {
-    return (
-      <div className="general">
-        <h3>feed me ur fit pics</h3>
-        <div>
-          <label className="label">
-            click here to upload
-            <input className="input" type="file" onChange={this.onFileChange} />
-          </label>
-        </div>
-        {this.fileData()}
-        <button onClick={this.onFileUpload}>i'm ready, roast me</button>
-      </div>
-    );
   }
+
+  return (
+    <div className="general">
+      <h3>feed me ur fit pics</h3>
+      <div>
+        <label className="label">
+          click here to upload
+          <input className="input" type="file" onChange={onFileChange} />
+        </label>
+      </div>
+      {fileData()}
+      <button onClick={onFileUpload}>i'm ready, roast me</button>
+    </div>
+  );
 }
 
 export default Upload;
