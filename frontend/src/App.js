7ecf9e18f2463fa66cb37501da758c1ae719ddd7
Peng Hao Lu <penglu@stanford.edu>
2021-04-11 02:52:33 -0400

    ready for backend test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Upload from "./components/Upload";
 
 function App() {
   return (
     <div className="App">
-      <div className="card">
-        <button>
-          UPLOAD
-        </button>
+      {/* <div className="card">
+        <button>UPLOAD</button>
       </div>
 
-      <button className="upload__btn">
-        UPLOAD
-      </button>
+      <button className="upload__btn">UPLOAD</button> */}
+
+      <h1 className="title">judge my fashion</h1>
+      <Upload />
     </div>
   );
 }
